a3029b013102c0499cac3ab48c89d0fa7ad39a6e
AmanDhawan <amandhawan672@gmail.com>
2023-10-20 14:32:22 +0530

    Added constraint and create city-Repository

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express=require("express");
 const {PORT}=require('./config/serverConfig');
 const bodyparser=require("body-parser");
+const CityRepository=require('./repository/city-repository');
 
 const setupAndStartServer=async()=>{
     const app=express();
     app.use(bodyparser.json());
     app.use(bodyparser.urlencoded({extended:true}));
-    app.listen(PORT,()=>{
+    app.listen(PORT,async()=>{
         console.log(`server starred at ${PORT}`);
+        
+
     });
 }
 setupAndStartServer();
